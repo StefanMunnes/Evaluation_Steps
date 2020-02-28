3ac5fd753b3e9eea28fe2d809e4ee09504e8ed2e
John Doe <munnes@systemli.org>
2020-02-28 09:25:07 +0100

    kick_off: Auswertungsskripte erstellt

---
+++ 3_Analyse/Scripte/kick_off.R
@@ -0,0 +1,120 @@
+######################################################
+# Skript zur Auswertung der KICK OFF Evaluation
+#
+# Stefan Munnes (munnes@wzb.eu)
+#
+######################################################
+
+
+### Präambel
+library(tidyverse)
+library(grid)
+library(gridExtra)
+library(jpeg)
+library(ggpubr)
+library(openxlsx)
+
+### Arbeitsverzeichnis setzen
+setwd("/home/steps/Evalutation_Steps")
+
+rm(list = ls())
+
+
+# externe Grafiken für den Report
+beberlin <- readJPEG("1_AMC/bbst_gs_deutsch/_pics/beberlin.jpg")
+steps    <- readJPEG("1_AMC/bbst_gs_deutsch/_pics/steps.jpg")
+
+
+# Labels
+varlab <- c(
+  "v1" = "1. Die Veranstaltung war klar und übersichtlich strukturiert.",
+  "v2" = "2. Die Veranstaltung versetzt mich in die Lage,\ndie Inhalte selbständig zu vertiefen.",
+  "v3" = "3. Das fachliche Niveau der Veranstaltung empfand\nich als angemessen.",
+  "v4" = "4. Das Erlernte und Erfahrene sind für mich beruflich von Nutzen.",
+  "v5" = "5. Es gab genügend Zeit für den allgemeinen bzw.\nfachlichen Austausch.",
+  "v6" = "6. Die Lehrveranstaltung begann und endete pünktlich.",
+  "v7" = "7. Die Organisation des Programms QuerBer wurde\nin der Begrüßungsveranstaltung verständlich dargestellt.",
+  "v8" = "8. Die Informationen waren über den Bildungsserver\nproblemlos abrufbar.",
+  "v9" = "9. ... hat Ziele und Struktur der Veranstaltung\nnachvollziehbar dargestellt.",
+  "v10" = "10. ... ging, soweit wie möglich, auf Wünsche und Fragen der\nTeilnehmenden ein und hat Anregungen aufgegriffen.",
+  "v11" = "11. ... gestaltete die Lehrveranstaltung interessant\nund abwechslungsreich.")
+
+notelab <- c("A" = 1, "B" = 2, "C" = 3, "D" = 4, "E" = 5, "F" = 6)
+
+partlab <- c("Zusammenfassende Einschätzung\nzur Veranstaltung", "Der Dozent/ die Dozentin ...")
+
+
+# Datensätze laden
+csvs <- str_replace(list.files("3_Analyse/Scans/kick_off/",
+                               pattern = "*.csv",
+                               include.dirs = T,
+                               recursive = T),
+                    ".csv", "")
+
+# lade alle (noch unbearbeiteten) CSV-Dateien in eine Liste als data.frames
+data.list <- sapply(csvs, function(x)
+                      read.csv2(paste0("3_Analyse/Scans/kick_off/", x, ".csv"),
+                                stringsAsFactor = T,
+                                colClasses = "character"),
+                   simplify = F,
+                   USE.NAMES = T)
+
+data.raw <- bind_rows(data.list, .id = "id")
+
+data.final <- data.raw %>%
+  mutate(Note = as.character(Note),
+         group = id) %>%
+  filter(Note != "-1") %>%
+  select(id, group, starts_with("TICKED.")) %>%
+  rename_at(vars(starts_with("TICKED.")), funs(str_replace(., "TICKED.", ""))) %>%
+  separate(id, c("kurs", "name", "zeit"), sep = "/") %>%
+  gather(frage, note, starts_with("v")) %>%
+  mutate(frage = recode_factor(frage, !!!varlab, .ordered = T),
+         note  = as.numeric(recode_factor(note, !!!notelab)),
+         part  = ifelse(grepl("(9|10|11)", frage), 2, 1),
+         part  = recode_factor(part, !!!partlab, .ordered = T))
+
+
+
+source("3_Analyse/Scripte/kick_off/ko_einzelberichte.R")
+source("3_Analyse/Scripte/kick_off/ko_personen.R")
+source("3_Analyse/Scripte/kick_off/ko_kurse.R")
+
+
+### Excel-Tabellen
+tbl.alle <- data.final %>%
+  group_by(group) %>%
+  summarise(Kurs = first(kurs),
+            Lehrperson = first(name),
+            Teilnehmende = n()/11,
+            Note = round(mean(note, na.rm = 1), 1)) %>%
+  select(-group)
+
+tbl.kurse <- data.final %>%
+  group_by(kurs) %>%
+  summarise(Kurskennung = first(kurs),
+            Teilnehmende = n()/11,
+            Note = round(mean(note, na.rm = 1), 1)) %>%
+  select(-kurs)
+
+tbl.personen <- data.final %>%
+  group_by(name) %>%
+  summarise(Lehrperson = first(name),
+            Teilnehmende = n()/11,
+            Note = round(mean(note, na.rm = 1), 1)) %>%
+  select(-name)
+
+excel <- createWorkbook("kick_off")
+
+addWorksheet(excel, "Einzelberichte")
+addWorksheet(excel, "Kurse")
+addWorksheet(excel, "Lehrpersonen")
+
+writeData(excel, "Einzelberichte", tbl.alle)
+writeData(excel, "Kurse", tbl.kurse)
+writeData(excel, "Lehrpersonen", tbl.personen)
+
+saveWorkbook(excel, "4_Ergebnisse/Tabellen/kick_off/Gesamtbericht.xlsx", overwrite = T)
+
+
+# exit
---
+++ 3_Analyse/Scripte/kick_off/ko_kurse.R
@@ -0,0 +1,87 @@
+
+for (x in unique(data.final$kurs)) {
+
+  data.temp <- data.final[data.final$kurs == x,]
+
+  file <- paste0("4_Ergebnisse/Grafiken/kick_off/Kurse/", x, ".pdf")
+
+  n_kurse <- length(unique(data.temp$group))
+
+  teilnehmende <- nrow(data.temp)/11
+
+  schnitt <- round(mean(data.temp[,"note"], na.rm = T), digits = 1)
+
+
+
+  par(oma = c(1, 1, 0, 1))
+  par(mar = c(1, 1, 0, 1))
+
+  plot(0:10, asp = 0.6, type = "n", xaxt = "n", yaxt = "n", bty = "n", xlab = "", ylab = "")
+
+  text(5.5, 7, "Feedback", cex = 2.2)
+  text(5.5, 6, "zum KICK OFF", cex = 1.5)
+  text(5.5, 5, x, cex = 1.2)
+
+  text(5.5, 3, paste0("Kurse: ", n_kurse), cex = 1)
+  text(5.5, 2, paste0("Teilnehmende: ", teilnehmende), cex = 1)
+  text(5.5, 1, paste0("Durchschnittliche Bewertung: ", schnitt), cex = 1.2)
+
+  rasterImage(beberlin, 1, 9, 4.5, 10)
+  rasterImage(steps, 7.5, 9 , 10, 10)
+
+  plot.title <- recordPlot()
+
+  dev.off()
+
+
+  plot.1 <- data.temp %>%
+    filter(grepl("(^(1. )|2|3|4|5|6|7|8)", frage), between(note, 1, 6)) %>%
+    ggplot(aes(y = note, x = reorder(frage, desc(frage)))) +
+      coord_flip() +
+      geom_count(color = alpha("mediumpurple3", 0.66), show.legend = F)  +
+      scale_size_area(max_size = 6) +
+      stat_summary(fun.y = mean, geom = "crossbar",
+                   aes(ymax = ..y..,
+                       ymin = ..y..),
+                   color = "indianred2",
+                   width = 0.5) +
+      scale_y_continuous(name = "",
+                         limits = c(1, 6),
+                         breaks = c(1, 2, 3, 4, 5, 6)) +
+      labs(title = "Zusammenfassende Einschätzung\nzur Veranstaltung", x = "") +
+      theme_light() +
+      theme(axis.text = element_text(size = 10),
+            plot.title = element_text(face = "bold", size = 12, colour = "black"))
+
+
+  plot.2 <- data.temp %>%
+    filter(grepl("(9|10|11)", frage), between(note, 1, 6)) %>%
+    ggplot(aes(y = note, x = reorder(frage, desc(frage)))) +
+      coord_flip() +
+      geom_count(color = alpha("mediumpurple3", 0.66), show.legend = F)  +
+      scale_size_area(max_size = 6) +
+      stat_summary(fun.y = mean, geom = "crossbar",
+                   aes(ymax = ..y..,
+                       ymin = ..y..),
+                   color = "indianred2",
+                   width = 0.5) +
+      scale_y_continuous(name = "",
+                         limits = c(1, 6),
+                         breaks = c(1, 2, 3, 4, 5, 6)) +
+      labs(title = "Der Dozent/ die Dozentin ...", x = "") +
+      theme_light() +
+      theme(axis.text = element_text(size = 10),
+            plot.title = element_text(face = "bold", size = 12, colour = "black"))
+
+
+  plot.graph <- ggarrange(plot.1, plot.2, ncol = 1, align = "v", heights = c(2, 1.1))
+
+  pdf(file, width = 8.3, height = 11.7, onefile=F)
+
+  print(ggarrange(plot.title, plot.graph, ncol = 1,  heights = c(1.6, 2), widths = c(2, 1.6)))
+
+  dev.off()
+
+}
+
+# exit
